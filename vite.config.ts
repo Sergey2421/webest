585b757a383728c5249da3ff200d9c786ebd70ee
sergey2421 <sergey_kakadey.@mail.ru>
2025-05-28 15:26:25 +0300

    env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  base: 'https://Sergey2421.github.io/webest/'
+
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [vue()],
+        base: process.env.BASE_URL,
+        define: {
+            'process.env.BASE_URL': JSON.stringify(env.BASE_URL)
+        }
+    }
 })
